7eb45b2b07baca72c09aa760e55aa4f85eddf95f
Yoshihiro Tanaka <contact@cordea.jp>
2022-03-20 11:13:58 +0900

    define repository

---
+++ lib/image_repository.dart
@@ -0,0 +1,24 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final imageRepositoryProvider = Provider((_) => ImageRepository());
+
+class ImageRepository {
+  static const _key = 'images';
+
+  Future<List<String>> find() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList(_key) ?? [];
+  }
+
+  Future<void> insert(String path) async {
+    final prefs = await SharedPreferences.getInstance();
+    final values = prefs.getStringList(_key) ?? [];
+    prefs.setStringList(_key, values + [path]);
+  }
+
+  Future<void> delete() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove(_key);
+  }
+}
